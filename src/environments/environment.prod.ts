7a0ba104d38f8a33e827d6e40f6d4e4aeef92784
gravielsosa <sosagraviel@gmail.com>
2023-09-09 09:50:38 -0300

    add new dockerfile

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  api: 'https://club-administration.onrender.com/api',
+  api: 'https://api.club-administration.qa.qubika.com/api',
   production: true,
   appVersion: '0.0.1'
 };
